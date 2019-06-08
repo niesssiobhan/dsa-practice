1692579f03ccd32f64731feec56b9a453496462f
niesssiobhan <niesssiobhan@live.com>
2019-06-07 18:29:08 -0700

    working on arrays

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -12,3 +12,9 @@ const uniqueChar = (str) => {
     }
     return true;
   }
+
+// Given two strings, write a method to decide if one is a permutation of the other. Example 
+
+const permutationCheck = (str1, str2) => {
+  return str1.split("").sort().join () === str2.split("").sort().join();
+};
--- test/arrays.test.js
+++ test/arrays.test.js
@@ -4,7 +4,7 @@ const arrays = require('../arrays.js');
 
 describe('uniqueChar', () => {
   it('should return true if all characters in the array are unique', () => {
-    const result = arrays.uniqueChar(['family']);
-    expect(result).toBe();
+    const result = arrays.uniqueChar(['f','a', 'm', 'i', 'l', 'y']);
+    expect(result).toBe(true);
   });
 });
